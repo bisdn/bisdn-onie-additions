85c31858be973742b2d7ccee9a1ea40a0973d68f
Jonas Gorski <jonas.gorski@bisdn.de>
2019-10-24 14:22:01 +0200

    mk_bisdn_yocto_image: do not hardcode x86_64 as ARCH
    Pass BISDN_ARCH to onie-mk-demo.sh instead of hardcoded x86_64.
    
    Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

--- mk_bisdn_yocto_image.sh
+++ mk_bisdn_yocto_image.sh
@@ -4,6 +4,7 @@
 [ -z "$CONSOLE_SPEED" ] &&  export CONSOLE_SPEED=115200
 [ -z "$CONSOLE_DEV" ] && export CONSOLE_DEV=1
 [ -z "$CONSOLE_PORT" ] && export CONSOLE_PORT=0x2f8
+[ -z "$BISDN_ARCH" ] && export BISDN_ARCH=x86_64
 
 [ -z "$BISDN_ONIE_MACHINE" ] && export BISDN_ONIE_MACHINE="unknown_machine"
 [ -z "$BISDN_ONIE_PLATFORM" ] && export BISDN_ONIE_PLATFORM="unknown_platform"
@@ -11,4 +12,4 @@
 MACHINE=$1
 shift
 
-./onie/build-config/scripts/onie-mk-demo.sh x86_64 ${BISDN_ONIE_MACHINE} ${BISDN_ONIE_PLATFORM} ./bisdn/installer/ ./bisdn/machine/${PLATFORM_VENDOR}/${MACHINE}/platform.conf onie-bisdn-${MACHINE}.bin OS $*
+./onie/build-config/scripts/onie-mk-demo.sh ${BISDN_ARCH} ${BISDN_ONIE_MACHINE} ${BISDN_ONIE_PLATFORM} ./bisdn/installer/ ./bisdn/machine/${PLATFORM_VENDOR}/${MACHINE}/platform.conf onie-bisdn-${MACHINE}.bin OS $*
