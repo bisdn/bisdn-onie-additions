2a53419e4ebe332d8a05f4c5bad1b71ae0700aa8
Roger Luethi <roger.luethi@bisdn.de>
2022-04-29 14:30:15 +0200

    installer/install.sh: handle empty disks
    A typical parted output seen by create_bisdn_linux_msdos_partition
    looks like this:
    
    --------------------------------------------------------
    ONIE:/ # parted -s -m /dev/sda unit s print
    BYT;
    /dev/sda:53742528s:scsi:512:512:msdos:ATA M.2 (S42) 3TE7:;
    1:2048s:262143s:260096s:ext2::;
    2:262144s:524287s:262144s:ext4::;
    3:524288s:2621439s:2097152s:ext4::;
    4:2621440s:15204351s:12582912s:ext4::boot;
    --------------------------------------------------------
    
    If there are no partitions, what we get in last_part is the second line
    from the output above (starting with /devv/sda). We need to handle this
    differently (as the gpt code already does).
    
    Signed-off-by: Roger Luethi <roger.luethi@bisdn.de>

--- bisdn/installer/install.sh
+++ bisdn/installer/install.sh
@@ -222,9 +222,15 @@ create_bisdn_linux_msdos_partition()
     # Find next available partition
     part_info="$(parted -s -m $blk_dev unit s print | tail -n 1)"
     last_part="$(echo -n $part_info | awk -F: '{print $1}')"
-    last_part_end="$(echo -n $part_info | awk -F: '{print $3}')"
-    # Remove trailing 's'
-    last_part_end=${last_part_end%s}
+    if expr "$last_part" : /dev/ >/dev/null; then
+      # Line starts with "/dev/", there are no partitions
+      last_part=0
+      last_part_end=0
+    else
+      last_part_end="$(echo -n $part_info | awk -F: '{print $3}')"
+      # Remove trailing 's'
+      last_part_end=${last_part_end%s}
+    fi
     part=$(( $last_part + 1 ))
     part_start=$(( $last_part_end + 1 ))
     # sectors_per_mb = (1024 * 1024) / 512 = 2048
