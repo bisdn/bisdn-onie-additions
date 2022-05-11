bisdn/bisdn-onie-additions

commit 41ebb6a312d312f409d212c10210b6916bb7b68f

u-boot-arch: move to generic installer
Replace the old installation code with appropriate callbacks to
the new installation script.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

bisdn/installer/u-boot-arch/install.sh
@@ -7,9 +7,7 @@
 
 set -e
 
-check_platform() {
-	/bin/true
-}
+BISDN_ENABLE_NOS_MODE=1
 
 install_uimage() {
     echo "Copying uImage to NOR flash:"
@@ -20,20 +18,59 @@ hw_load() {
     echo "cp.b $img_start \$loadaddr $img_sz"
 }
 
-platform_install()
+platform_erase_disk()
 {
-    cd $(dirname $0)
-    . ./machine.conf
+    local blk_dev="$1"
+    local oIFS part parts part_num part_label
 
-    echo "BISDN Linux Installer: platform: $platform"
+    oIFS=$IFS
+    IFS="
+"
+    # remove all other NOS partitions
 
-    . ./platform.conf
+    # ugly: find a better way to get only partitions
+    parts="$(sgdisk -p /dev/sda | grep '^ ' || true)"
+    for part in $parts; do
+        part_num="$(echo $part | awk '{print $1}')"
+        part_label="$(echo $part | awk '{print $7}')"
 
-    check_platform
+        # keep diag partition intact
+        [ -n "$DIAG_PART_NAME" ] && [ "$part_label" = "$DIAG_PART_NAME" ] && continue
+        echo "removing partition $blk_dev$part_num ($part_label)"
+        sgdisk -d $part_num $blk_dev || {
+           echo "Error: unable to delete partition $part_num on $blk_dev"
+           exit 1
+        }
+    done
+    IFS=$oIFS
+    partprobe
+}
 
-    install_uimage
+platform_get_firmware_type()
+{
+	echo "u-boot"
+}
 
-    hw_load_str="$(hw_load)"
+. ./platform.conf
+
+platform_install_bootloader_entry()
+{
+    local blk_dev=$1
+    local bisdn_linux_part=$2
+    local bisdn_linux_mnt=$3
+
+    if [ -f fitImage ]; then
+	cp fitImage $bisdn_linux_mnt/boot/uImage
+    fi
+
+    if [ ! -f $bisdn_linux_mnt/boot/uImage ]; then
+        echo "Error: No kernel image in root fs"
+        exit 1
+    fi
+
+    machine_fixups
+
+    hw_load_str="$(hw_load $blk_dev $bisdn_linux_part)"
 
     echo "Updating U-Boot environment variables"
     (cat <<EOF
@@ -44,13 +81,4 @@ EOF
     ) > /tmp/env.txt
 
     fw_setenv -f -s /tmp/env.txt
-
-    cd /
-
-    # Set NOS mode if available.  For manufacturing diag installers, you
-    # probably want to skip this step so that the system remains in ONIE
-    # "installer" mode for installing a true NOS later.
-    if [ -x /bin/onie-nos-mode ] ; then
-        /bin/onie-nos-mode -s
-    fi
 }

bisdn/machine/accton/accton-as4610/platform.conf
@@ -2,7 +2,10 @@
 
 # over ride default behaviour
 
-check_platform() {
+DIAG_PART_NAME="ACCTON-DIAG"
+BISDN_PART_SIZE=1024
+
+platform_check() {
     local platform=$(onie-syseeprom -g 0x21)
     local onie_version=$(onie-sysinfo -v)
 
@@ -30,7 +33,7 @@ check_platform() {
     return 0
 }
 
-getonboard_block_device() {
+platform_detect_boot_device() {
     blk_dev=
     for i in a b; do
         [ -n "$(ls -l /sys/block/sd${i}/device 2>/dev/null | grep '1-2.3:1.0')" ] && {
@@ -54,170 +57,8 @@ getonboard_block_device() {
 
 bisdn_linux_part=
 
-create_gpt_partition() {
-    local blk_dev="$1"
-    local label="$2"
-    local size="$3"
-    local last_part part attr_bitmask
-
-    # fixup early MBR partitions
-    sgdisk -v $blk_dev | grep -q "converting MBR" && sgdisk -g $blk_dev
-
-    part=$(sgdisk -p $blk_dev | grep "$2" | awk '{print $1}')
-    if [ -n "$part" ]; then
-        # TODO: backup existing config
-        sgdisk -d $part $blk_dev || {
-           echo "Error: Unable to delete partition $part on $blk_dev"
-           exit 1
-        }
-        partprobe
-    else
-	local oIFS parts part_num part_label
-
-	oIFS=$IFS
-	IFS="
-"
-	# remove all other NOS partitions
-
-	# ugly: find a better way to get only partitions
-	parts="$(sgdisk -p /dev/sda | grep '^ ' || true)"
-	for part in $parts; do
-		part_num="$(echo $part | awk '{print $1}')"
-		part_label="$(echo $part | awk '{print $7}')"
-
-		# keep diag partition intact
-		[ "$part_label" = "ACCTON-DIAG" ] && continue
-		echo "removing partition $blk_dev$part_num ($part_label)"
-		sgdisk -d $part_num $blk_dev || {
-			echo "Error: unable to delete partition $part_num on $blk_dev"
-			exit 1
-		}
-	done
-        IFS=$oIFS
-	partprobe
-    fi
-
-    last_part=$(sgdisk -p $blk_dev | tail -n 1 | awk '{print $1}')
-    # empty table will catch the header instead
-    [ "$last_part" = "Number" ] && last_part=0
-    part=$(( $last_part + 1 ))
-
-    # Create new partition
-    echo "Creating new partition ${blk_dev}$part ..."
-
-    attr_bitmask="0x0"
-    sgdisk --new=${part}::+${size}MB \
-        --attributes=${part}:=:$attr_bitmask \
-        --change-name=${part}:$label $blk_dev || {
-        echo "Error: Unable to create partition $part on $blk_dev"
-        exit 1
-    }
-    partprobe
-    bisdn_linux_part=$part
-    bisdn_linux_uuid="$(echo -e "i\n${bisdn_linux_part}\nq" | gdisk $blk_dev | grep "^Partition unique" | awk '{print $4}')"
-}
-
-create_msdos_partition()
-{
-    local blk_dev="$1"
-    local label="$2"
-    local size="$3"
-    local sectors_per_mb=2048
-    local last_part part part_info last_part_end part_start part_end
-
-    # See if BISDN Linux partition already exists -- look for the filesystem
-    # label.
-    part_info="$(blkid | grep $label | awk -F: '{print $1}')"
-    if [ -n "$part_info" ] ; then
-        # delete existing partition
-        part="$(echo -n $part_info | sed -e s#${blk_dev}##)"
-        parted -s $blk_dev rm $bisdn_linux_part || {
-            echo "Error: Unable to delete partition $part on $blk_dev"
-            exit 1
-        }
-        partprobe
-    fi
-
-    # Find next available partition
-    part_info="$(parted -s -m $blk_dev unit s print | tail -n 1)"
-    last_part="$(echo -n $part_info | awk -F: '{print $1}')"
-    last_part_end="$(echo -n $part_info | awk -F: '{print $3}')"
-    # Remove trailing 's'
-    last_part_end=${last_part_end%s}
-    part=$(( $last_part + 1 ))
-    part_start=$(( $last_part_end + 1 ))
-    # sectors_per_mb = (1024 * 1024) / 512 = 2048
-    sectors_per_mb=2048
-    part_end=$(( $part_start + ( $size * $sectors_per_mb ) - 1 ))
-
-    # Create new partition
-    echo "Creating new BISDN Linux partition ${blk_dev}$bisdn_linux_part ..."
-    parted -s --align optimal $blk_dev unit s \
-      mkpart primary $part_start $part_end set $part boot on || {
-        echo "ERROR: Problems creating BISDN Linux msdos partition $part on: $blk_dev"
-        exit 1
-    }
-    partprobe
-    bisdn_linux_part=$part
-}
-
-install_uimage() {
+machine_fixups() {
     local bootcmd
-
-    bisdn_linux_volume_label="BISDN-Linux"
-
-    # determine ONIE partition type
-    onie_partition_type=$(onie-sysinfo -t)
-
-    if [ "$onie_partition_type" = "gpt" ] ; then
-        create_bisdn_partition="create_gpt_partition"
-    elif [ "$onie_partition_type" = "msdos" ] ; then
-        create_bisdn_partition="create_msdos_partition"
-    else
-       echo "ERROR: Unsupported partition type: $onie_partition_type"
-       exit 1
-    fi
-
-    [ -n $DEBUG ] && echo "DEBUG: onie_partition_type=${onie_partition_type}"
-
-    blk_dev=$(getonboard_block_device)
-    fs_type=ext4
-
-    eval $create_bisdn_partition $blk_dev $bisdn_linux_volume_label 1024
-    [ -n "$bisdn_linux_part" ] || exit 1
-
-    bisdn_linux_dev=${blk_dev}${bisdn_linux_part}
-    mkfs.$fs_type -L $bisdn_linux_volume_label $bisdn_linux_dev || {
-      echo "Error: unable to create BISDN Linux file system on $bisdn_linux_dev"
-      exit 1
-    }
-
-    # copy stuff to partition
-    bisdn_linux_mnt=$(mktemp -d) || {
-      echo "Error: unable to create BISDN Linux file system mount point"
-      exit 1
-    }
-
-    mount -t $fs_type -o defaults,rw $bisdn_linux_dev $bisdn_linux_mnt || {
-      echo "Error: unable to mount $bisdn_linux_dev on $bisdn_linux_mnt"
-      exit 1
-    }
-
-    if [ -f rootfs.cpio.gz ]; then
-        image_archive=$(realpath rootfs.cpio.gz)
-        cd $bisdn_linux_mnt
-        zcat $image_archive | cpio -i
-    elif [ -f rootfs.tar.xz ]; then
-        xzcat rootfs.tar.xz | tar xf - -C $bisdn_linux_mnt
-    else
-        echo "Error: invalid rootfs"
-    fi
-
-    [ -f fitImage ] && cp fitImage $bisdn_linux_mnt/boot/uImage
-
-    onie-support $bisdn_linux_mnt
-    sync
-    umount $bisdn_linux_mnt || echo "Error: failed to unmount $bisdn_linux_mnt"
 
     # ugly, fixup bootcmd from picos to nos
     bootcmd=$(fw_printenv bootcmd)
@@ -235,7 +76,11 @@ install_uimage() {
 }
 
 hw_load() {
+    local blk_dev=$1
+    local bisdn_linux_part=$2
     local platform=$(onie-syseeprom -g 0x21)
+
+    bisdn_linux_uuid="$(echo -e "i\n${bisdn_linux_part}\nq" | gdisk $blk_dev | grep "^Partition unique" | awk '{print $4}')"
 
     case "$platform" in
     "4610-30T"*)

commit 65c2e65d944e2caa1fa60f8e653c6e0de9e7c898

u-boot-arch/install.sh: remove unused code
Remove the install_uimage fuction which is no longer
used anywhere.

Fixes: 41ebb6a312d312f409d212c10210b6916bb7b68f

Signed-off-by: Roger Luethi <roger.luethi@bisdn.de>

bisdn/installer/u-boot-arch/install.sh
@@ -12,11 +12,6 @@ set -e
 
 BISDN_ENABLE_NOS_MODE=1
 
-install_uimage() {
-    echo "Copying uImage to NOR flash:"
-    flashcp -v bisdn-linux-${platform}.itb $mtd_dev
-}
-
 hw_load() {
     echo "cp.b $img_start \$loadaddr $img_sz"
 }